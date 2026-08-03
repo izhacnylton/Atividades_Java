9726768066d104f0ab5b7111ab8fb851a7af9ec3
Izhac Nylton <izhac2006@gmail.com>
2026-08-03 14:22:22 -0400

    1 e 2

---
+++ src/ValidacaoDeSenha.java
@@ -0,0 +1,9 @@
+
+public class ValidacaoDeSenha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/tabuadaDinamica.java
+++ src/tabuadaDinamica.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class tabuadaDinamica {
 
 	public static void main(String[] args) {
-		int num;
+		int num01 = 0;
+		int resultado = 0;
 		
 		Scanner sc = new Scanner(System.in);
-		Scanner = 
+		System.out.println("Insira um número de 1 a 10: ");
+		num01 = sc.nextInt();
+		if(num01 >= 1 && num01 <= 10) {
+			for(int i = 1; i < 11; i++) {
+				resultado = i * num01;
+				System.out.println(num01 + " x " + i + " = " + resultado);
+			}
+		}
+		else {
+			System.out.println("Numero Invalido");
+		}
 
 	}
 
